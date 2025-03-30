6420b3a4478fd9d7d45e1ab49714e2fb74fbf772
Yan Carlos <yan312005@gmail.com>
2025-03-30 16:37:47 -0300

    fix: URL of the deployed API

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://contact-management-api-wcsq.onrender.com",
   withCredentials: true,
 });
-
-// api.interceptors.request.use(async (config) => {
-//   await new Promise((resolve) =>
-//     setTimeout(resolve, Math.round(Math.random() * 3000)),
-//   );
-//   return config;
-// });
